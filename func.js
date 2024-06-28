0c61c5490e306753bd9f9b6691e52d1321b826fd
twesigetrejan <trejantwesige19@gmail.com>
2024-06-29 00:06:01 +0300

    corrected something

--- func.js
+++ func.js
@@ -1,126 +1,99 @@
 const scoreCount = document.querySelector("#scoreCount");
-const paperBtn = document.querySelector("#paper")
+const paperBtn = document.querySelector("#paper");
 const scissorBtn = document.querySelector("#scissors");
 const rockBtn = document.querySelector("#rock");
+const result = document.getElementById('result');
+const word_result = document.getElementById('word_result');
+const gameplayer = document.getElementById('gameplayer');
 
+let count = scoreCount.textContent;
+let score = Number(count);
 
-//const user_choices = document.querySelectorAll(".choice");
-
-// console.log(user_choices);
-
-scoreCount.innerHTML = 0;
+scoreCount.innerHTML = score;
 
 const possibilities = ["rock", "paper", "scissor"];
 let userChoice = "";
-let botChoice = ""
+let botChoice = "";
 
-function returnWinner(userChoice){
-    botChoice = possibilities[Math.floor(Math.random() * possibilities.length)]
-    console.log(`User:${userChoice}<<<<<>>>>>Bot: ${botChoice}`);
+function returnWinner(userChoice) {
+  botChoice = possibilities[Math.floor(Math.random() * possibilities.length)];
+  console.log(`User:${userChoice} <<<<<>>>>> Bot: ${botChoice}`);
 
-    if(userChoice == botChoice){
-        console.log("Draw");
-    }else if(userChoice == "paper"){
-        if(botChoice == "rock"){
-            console.log("win");
-        }else{
-            console.log("lose");
-        }
-    }else if(userChoice == "rock"){
-        if (botChoice == "scissor") {
-          console.log("win");
-        } else {
-          console.log("lose");
-        }
-    }else{
-        if (botChoice == "paper") {
-          console.log("win");
-        } else {
-          console.log("lose")
+  if (userChoice === botChoice) {
+    console.log("Draw");
+    word_result.innerHTML = "THAT WAS A DRAW 🤝";
+
+  } else if (userChoice === "paper") {
+    if (botChoice === "rock") {
+      console.log("win");
+      word_result.innerHTML = "YOU WIN 🏆";
+      score++;
+      scoreCount.innerHTML = score;
+    } else {
+      console.log("lose");
+      word_result.innerHTML = "YOU LOST 🤣";
     }
-}
+  } else if (userChoice === "rock") {
+    if (botChoice === "scissor") {
+      console.log("win");
+      word_result.innerHTML = "YOU WIN 🏆";
+      score++;
+      scoreCount.innerHTML = score;
+    } else {
+      console.log("lose");
+      word_result.innerHTML = "YOU LOST 🤣";
+    }
+  } else {
+    if (botChoice === "paper") {
+      console.log("win");
+      word_result.innerHTML = "YOU WIN 🏆";
+      score++;
+      scoreCount.innerHTML = score;
+    } else {
+      console.log("lose");
+      word_result.innerHTML = "YOU LOST 🤣";
+    }
+  }
+
+
+  const HumanImageChoice = document.getElementById('HumanImageChoice');
+  if (userChoice === "paper") {
+    HumanImageChoice.src = '/icons/paper.png';
+  } else if (userChoice === "scissor") {
+    HumanImageChoice.src = '/icons/scissors.png';
+  } else if (userChoice === "rock") {
+    HumanImageChoice.src = '/icons/rock.png';
+  }
+
+  const BotImageChoice = document.getElementById('BotImageChoice');
+  if (botChoice === "paper") {
+    BotImageChoice.src = '/icons/paper.png';
+  } else if (botChoice === "scissor") {
+    BotImageChoice.src = '/icons/scissors.png';
+  } else if (botChoice === "rock") {
+    BotImageChoice.src = '/icons/rock.png';
+  }
+
+
+  result.classList.remove("hidden");
 }
 
-//noble
+
 paperBtn.addEventListener("click", () => {
-    userChoice = "paper"
-    returnWinner(userChoice);
+  userChoice = "paper";
+  returnWinner(userChoice);
+});
 
-})
 rockBtn.addEventListener("click", () => {
   userChoice = "rock";
   returnWinner(userChoice);
 });
+
 scissorBtn.addEventListener("click", () => {
   userChoice = "scissor";
   returnWinner(userChoice);
 });
 
-
-
-
-
-
-// This is Shafic
-
-// const weapons = {
-//     rock: {
-//         wins: "scissors"
-//     },
-//     scissors: {
-//         wins: "paper"
-//     },
-//     paper: {
-//         wins: "rock"
-//     }
-// }
-
-
-// user_choices.forEach((choiceBtn) => {
-//   choiceBtn.addEventListener("click", (event) => {
-
-//     let possibilities = Object.keys(weapons);
-//     userChoice = choiceBtn.id;
-//     botChoice = possibilities[Math.floor(Math.random() * possibilities.length)];
-
-
-//     if(userChoice === botChoice){
-//         console.log ("this is draw ", `BOT: ${botChoice} -`, `- PLAYER: ${userChoice}` )
-        
-//     }else if(weapons[userChoice].wins == botChoice){
-//         console.log("YOU WON", `BOT: ${botChoice} -`, `- PLAYER: ${userChoice}`)
-//     }else{
-//         console.log(
-//           "YOU LOST",
-//           `BOT: ${botChoice} -`,
-//           `- PLAYER: ${userChoice}`
-//         );
-//     }
-
-
-//   });
-// });
-
-// SIR MARTIN ---
-// paperBtn.addEventListener("click", () => {
-//   console.log("USER: paper");
-//   let botChoice = possibilities[Math.floor(Math.random() * possibilities.length)];
-//   console.log(botChoice);
-
-//   if(){
-
-//   }
-
-// });
-
-// scissorsBtn.addEventListener("click", () => {
-//   console.log("USER: scissor");
-//   let botChoice = Math.floor(Math.random() * possibilities.length);
-//   console.log(possibilities[botChoice]);
-// });
-
-// rockBtn.addEventListener("click", () => {
-//   console.log("USER: rock");
-//   let botChoice = Math.floor(Math.random() * possibilities.length);
-//   console.log(possibilities[botChoice]);
-// });
+gameplayer.addEventListener('click', () => {
+  result.classList.add("hidden");
+});
