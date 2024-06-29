ee4729ff4cdb6e730642cbfbc51fa7261a9dcbe6
Sir Martin <msamuel25600@gmail.com>
2024-06-29 09:08:59 +0300

    initial commit(displaying result card)

--- func.js
+++ func.js
@@ -4,56 +4,63 @@ const scissorBtn = document.querySelector("#scissors");
 const rockBtn = document.querySelector("#rock");
 
 
-//const user_choices = document.querySelectorAll(".choice");
+const user_choices = document.querySelectorAll(".choice");
+//
+const resultScreen = document.querySelector(".result");
+const gameScreen = document.querySelector(".gameScreen");
+const resultMessage = document.querySelector(".resultMessage");
+const playAgain = document.querySelector(".playAgain");
+const userChoiceImg = document.querySelector(".userChoiceImg img");
+const botChoiceImg = document.querySelector(".botChoiceImg img");
+
+//
 
 // console.log(user_choices);
 
-scoreCount.innerHTML = 0;
+// scoreCount.innerHTML = 0;
 
-const possibilities = ["rock", "paper", "scissor"];
-let userChoice = "";
-let botChoice = ""
+// const possibilities = ["rock", "paper", "scissor"];
+// let userChoice = "";
+// let botChoice = ""
 
-function returnWinner(userChoice){
-    botChoice = possibilities[Math.floor(Math.random() * possibilities.length)]
-    console.log(`User:${userChoice}<<<<<>>>>>Bot: ${botChoice}`);
+// function returnWinner(userChoice){
+//     botChoice = possibilities[Math.floor(Math.random() * possibilities.length)]
+//     console.log(`User:${userChoice}<<<<<>>>>>Bot: ${botChoice}`);
 
-    if(userChoice == botChoice){
-        console.log("Draw");
-    }else if(userChoice == "paper"){
-        if(botChoice == "rock"){
-            console.log("win");
-        }else{
-            console.log("lose");
-        }
-    }else if(userChoice == "rock"){
-        if (botChoice == "scissor") {
-          console.log("win");
-        } else {
-          console.log("lose");
-        }
-    }else{
-        if (botChoice == "paper") {
-          console.log("win");
-        } else {
-          console.log("lose")
-    }
-}
-}
+//     if(userChoice == botChoice){
+//         console.log("Draw");
+//     }else if(userChoice == "paper"){
+//         if(botChoice == "rock"){
+//             console.log("win");
+//         }else{
+//             console.log("lose");
+//         }
+//     }else if(userChoice == "rock"){
+//         if (botChoice == "scissor") {
+//           console.log("win");
+//         } else {
+//           console.log("lose");
+//         }
+//     }else{
+//         if (botChoice == "paper") {
+//           console.log("win");
+//         } else {
+//           console.log("lose")
+//     }
+// }
+// }
 
-//noble
+// //noble
 paperBtn.addEventListener("click", () => {
-    userChoice = "paper"
-    returnWinner(userChoice);
+    userChoiceImg.src = "/icons/paper.png";
 
 })
 rockBtn.addEventListener("click", () => {
-  userChoice = "rock";
-  returnWinner(userChoice);
+  userChoiceImg.src = "/icons/rock.png";
+  botChoiceImg.src = "/icons/rock.png";
 });
 scissorBtn.addEventListener("click", () => {
-  userChoice = "scissor";
-  returnWinner(userChoice);
+  userChoiceImg.src = "/icons/scissors.png";
 });
 
 
@@ -63,43 +70,69 @@ scissorBtn.addEventListener("click", () => {
 
 // This is Shafic
 
-// const weapons = {
-//     rock: {
-//         wins: "scissors"
-//     },
-//     scissors: {
-//         wins: "paper"
-//     },
-//     paper: {
-//         wins: "rock"
-//     }
-// }
+const weapons = {
+    rock: {
+        wins: "scissors"
+    },
+    scissors: {
+        wins: "paper"
+    },
+    paper: {
+        wins: "rock"
+    }
+}
+
+let count = 0;
+scoreCount.innerHTML = count;
 
 
-// user_choices.forEach((choiceBtn) => {
-//   choiceBtn.addEventListener("click", (event) => {
+user_choices.forEach((choiceBtn) => {
+  choiceBtn.addEventListener("click", (event) => {
 
-//     let possibilities = Object.keys(weapons);
-//     userChoice = choiceBtn.id;
-//     botChoice = possibilities[Math.floor(Math.random() * possibilities.length)];
+    resultScreen.classList.remove("hidden")
+    gameScreen.classList.add("hidden")
 
 
-//     if(userChoice === botChoice){
-//         console.log ("this is draw ", `BOT: ${botChoice} -`, `- PLAYER: ${userChoice}` )
+    let possibilities = Object.keys(weapons);
+    console.log(possibilities)
+    userChoice = choiceBtn.id;
+    botChoice = possibilities[Math.floor(Math.random() * possibilities.length)];
+
+    //
+if(botChoice =="rock"){
+  botChoiceImg.src = "/icons/rock.png";
+}else if(botChoice == "paper"){
+  botChoiceImg.src = "/icons/paper.png";
+}else{
+  botChoiceImg.src = "/icons/scissors.png";
+}
+    //
+
+    if(userChoice === botChoice){
+      resultMessage.innerHTML = "THIS IS A DRAW" 
+        console.log ("this is draw ", `BOT: ${botChoice} -`, `- PLAYER: ${userChoice}` )
         
-//     }else if(weapons[userChoice].wins == botChoice){
-//         console.log("YOU WON", `BOT: ${botChoice} -`, `- PLAYER: ${userChoice}`)
-//     }else{
-//         console.log(
-//           "YOU LOST",
-//           `BOT: ${botChoice} -`,
-//           `- PLAYER: ${userChoice}`
-//         );
-//     }
+    }else if(weapons[userChoice].wins == botChoice){
+      resultMessage.innerHTML = "YOU WON";
+      count+=5;
+      console.log(count)
+      scoreCount.innerHTML = count;
+        console.log("YOU WON", `BOT: ${botChoice} -`, `- PLAYER: ${userChoice}`)
+    }else{
+      resultMessage.innerHTML = "YOU LOST"
+      count-=5;
+      console.log(count) 
+      scoreCount.innerHTML = count;
+        console.log(
+          "YOU LOST",
+          `BOT: ${botChoice} -`,
+          `- PLAYER: ${userChoice}`
+        );
+    }
 
 
-//   });
-// });
+  });
+});
 
 // SIR MARTIN ---
 // paperBtn.addEventListener("click", () => {
@@ -124,3 +157,42 @@ scissorBtn.addEventListener("click", () => {
 //   let botChoice = Math.floor(Math.random() * possibilities.length);
 //   console.log(possibilities[botChoice]);
 // });
+
+
+
+playAgain.addEventListener("click", ()=>{
+    resultScreen.classList.add("hidden")
+    gameScreen.classList.remove("hidden")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+      }
+    }
+  }
+}
+
